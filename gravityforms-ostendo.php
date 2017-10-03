4373f785e6d95be8c42a6520fd3156623709d16b
Adrien Goulet <ace@acegoulet.com>
2017-10-03 13:38:31 -0700

    small adjustment to link

--- gravityforms-ostendo.php
+++ gravityforms-ostendo.php
@@ -8,7 +8,7 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://sanbornagency.com
+ * @link              https://www.acegoulet.com
  * @since             1.0.0
  * @package           Gravityforms_Ostendo
  *
